9ba212a0101820d0137b5f12c9475ba32116072d
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-04 14:03:46 -0600

    forms setup in RecipeCreate

--- src/components/RecipeCreate/RecipeCreate.js
+++ src/components/RecipeCreate/RecipeCreate.js
@@ -5,7 +5,11 @@ export const RecipeCreate = () => {
   return (
     <div>
         <form>
-            
+            <input placeholder="Recipe Title"></input>
+            <input placeholder="Cook Time"></input>
+            <input placeholder="Difficult (true/false)"></input>
+            <input placeholder="Ingredients"></input>
+            <input placeholder="Instructions"></input>
         </form>
     </div>
   )
