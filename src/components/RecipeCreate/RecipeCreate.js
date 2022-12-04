6629588120e4f909b61a1e07152fdb50992f48f4
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-04 14:00:52 -0600

    Added RecipeCreate for create request

---
+++ src/components/RecipeCreate/RecipeCreate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RecipeCreate.css'
+
+export const RecipeCreate = () => {
+  return (
+    <div>
+        <form>
+            
+        </form>
+    </div>
+  )
+}
